964d9b96ed36b07d14e0ef92b5105a18c8af48a3
iamnbd2022 <iamnbd2022@gmail.com>
2023-04-15 09:51:29 +1000

    Added dns js for bianya

---
+++ bianya.ian.js
@@ -0,0 +1,4 @@
+(function(){
+   let ianDns = ' { "version": "0.0.1", "host": "bianya.ian", "title": "bianya.ian", "lut": 1669976019, "endpoints": [ { "endpoint": "https://ka5fr30c4f.execute-api.us-east-1.amazonaws.com", "lut": 1664272725 } ] }';
+   window.ianDns = ianDns;
+}());
